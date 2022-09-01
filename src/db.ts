1fde433e66a84ae2d2641cb5253236131ee95729
Gerard Keiser <gerardkeiser@gmail.com>
2022-09-01 17:56:37 -0500

    Export db from db.ts

--- src/db.ts
+++ src/db.ts
@@ -1,2 +1,19 @@
 /** Database setup for BizTime. */
 
+import { Client } from "pg";
+
+const db = connectDB(process.env.NODE_ENV);
+
+export { db };
+
+/*
+ * Helpers
+ */
+
+function connectDB(env: string | undefined) {
+  const prefix = "postgresql:///";
+  const DB_URI = env === "test" ? prefix + "bizime_test" : prefix + "biztime";
+  const db = new Client({ connectionString: DB_URI });
+  db.connect();
+  return db;
+}
