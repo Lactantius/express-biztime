e5287155eb2f7e80374686699278b7c694ff26d7
Gerard Keiser <gerardkeiser@gmail.com>
2022-09-02 16:31:11 -0500

    Add tables and seed data for industries

--- __tests__/test_data.sql
+++ __tests__/test_data.sql
@@ -1,5 +1,7 @@
+DROP TABLE IF EXISTS industries_companies;
 DROP TABLE IF EXISTS invoices;
 DROP TABLE IF EXISTS companies;
+DROP TABLE IF EXISTS industries;
 
 CREATE TABLE companies (
     code text PRIMARY KEY,
@@ -17,12 +19,34 @@ CREATE TABLE invoices (
     CONSTRAINT invoices_amt_check CHECK ((amt > (0)::double precision))
 );
 
+CREATE TABLE industries(
+    code text PRIMARY KEY,
+    name text NOT NULL UNIQUE
+);
+
+CREATE TABLE industries_companies (
+    id serial PRIMARY KEY,
+    comp_code text NOT NULL REFERENCES companies ON DELETE CASCADE,
+    ind_code text NOT NULL REFERENCES industries ON DELETE CASCADE
+);
+
 INSERT INTO companies
   VALUES ('apple', 'Apple Computer', 'Maker of OSX.'),
          ('ibm', 'IBM', 'Big blue.');
 
-INSERT INTO invoices (comp_Code, amt, paid, paid_date)
+INSERT INTO invoices (comp_code, amt, paid, paid_date)
   VALUES ('apple', 100, false, null),
          ('apple', 200, false, null),
          ('apple', 300, true, '2018-01-01'),
          ('ibm', 400, false, null);
+
+INSERT INTO industries (code, name)
+  VALUES ('it', 'Business IT'),
+         ('software', 'Software'),
+         ('goods', 'Consumer Goods');
+
+INSERT INTO industries_companies (comp_code, ind_code)
+  VALUES ('ibm', 'it'),
+         ('ibm', 'software'),
+         ('apple', 'goods'),
+         ('apple', 'software');
